1be3fd84ff4c29d747b34fa2ba73c892960455e8
Baiur Yuliia <juliy14497@gmail.com>
2024-05-10 20:09:49 +0300

    Add command

---
+++ README.md
@@ -0,0 +1,11 @@
+Weather telegram bot
+
+```
+/start - start command 
+
+/site - go to website 'sinoptik.ua'
+
+/weather - show the weather in your city
+
+/temperature - show temperature in your city a certain period of time
+```
--- weather_bot.py
+++ weather_bot.py
@@ -1,24 +1,39 @@
 import telebot
+import webbrowser
+
 from config import secret_token
 
-API_TOKEN = secret_token
 
+API_TOKEN = secret_token
 bot = telebot.TeleBot(API_TOKEN)
 
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, f'Привет, {message.from_user.first_name}')
+    bot.send_message(message.chat.id, f'Привет, {message.from_user.first_name}!')
+
+
+@bot.message_handler(commands=['site', 'website'])
+def site(message):
+    webbrowser.open('https://sinoptik.ua/')
+
+
+@bot.message_handler(commands=['weather'])
+def weather(message):
+    bot.send_message(message.chat.id, 'Погода сегодня: ясно')
+
+
+@bot.message_handler(commands=['temperature'])
+def weather(message):
+    bot.send_message(message.chat.id, 'Температура сейчас: тепло')
 
 
 @bot.message_handler()
 def info(message):
     if message.text.lower() == 'привет':
         bot.send_message(message.chat.id, f'Привет, {message.from_user.first_name}')
-    elif message.text.lower() == 'id':
-        bot.reply_to(message, f'ID: {message.from_user.id}')
     else:
-        bot.send_message(message.chat.id, """Я вас не понимаю. """)
+        bot.send_message(message.chat.id, """Я вас не понимаю. Попробуйте еще раз. """)
 
 
 bot.infinity_polling()
